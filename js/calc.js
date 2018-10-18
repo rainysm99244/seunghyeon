40732ea9d7d2cd07022ba670e16bfdf7eb04ab51
rainysm99244 <43920973+rainysm99244@users.noreply.github.com>
2018-10-18 22:13:33 +0900

    Add files via upload

--- js/calc.js
+++ js/calc.js
@@ -1,12 +1,17 @@
 window.onload = function() {
 
-    setInterval(calcDay, 200);
+    setInterval(calcDay, 1000);
 };
 
 function calcDay(){
     var startDate = new Date('1999-11-13');
     var todayDate = new Date();
 
-    document.getElementById("mosol").innerHTML =  "승현이는 오늘로 " + (parseInt((todayDate.getTime() - startDate.getTime()) / 1000 / 60 / 60 / 24)).toString() + "일째 모쏠";
+    var day = (parseInt((todayDate.getTime() - startDate.getTime()) / 1000 / 60 / 60 / 24)).toString();
+    var hour = (parseInt((todayDate.getTime() - startDate.getTime()) / 1000 / 60 / 60 % 24 )).toString();
+    var minute = (parseInt((todayDate.getTime() - startDate.getTime()) / 1000 / 60 % 60 )).toString();
+    var second = (parseInt((todayDate.getTime() - startDate.getTime()) / 1000 % 60  )).toString();
+
+    document.getElementById("mosolDay").innerHTML =  "승현이는 오늘로 " + day + "일 " + hour + "시간 " + minute + "분 " + second + "초 째 모쏠";
 
 }
