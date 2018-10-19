afbf2a668a433fc6ca19fe37d64be74b98368589
rainysm99244 <43920973+rainysm99244@users.noreply.github.com>
2018-10-19 11:46:26 +0900

    Add files via upload

--- js/calc.js
+++ js/calc.js
@@ -12,6 +12,6 @@ function calcDay(){
     var minute = (parseInt((todayDate.getTime() - startDate.getTime()) / 1000 / 60 % 60 )).toString();
     var second = (parseInt((todayDate.getTime() - startDate.getTime()) / 1000 % 60  )).toString();
 
-    document.getElementById("mosolDay").innerHTML =  "승현이는 오늘로 " + day + "일 " + hour + "시간 " + minute + "분 " + second + "초 째 모쏠";
+    document.getElementById("mosolDay").innerHTML = day + "일 " + hour + "시간 " + minute + "분 " + second + "초 째 모쏠";
 
 }
